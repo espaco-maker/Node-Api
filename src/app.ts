004c5dcc9981bdfc61623468ede5ef80b7031823
BhryanS2 <bhryanstepenhen@gmail.com>
2022-01-10 18:20:18 -0300

    subindo para o heroku

--- src/app.ts
+++ src/app.ts
@@ -12,15 +12,4 @@ const serverHttp = http.createServer(app);
 app.use(express.json());
 app.use(router);
 
-app.get("/github", (req, res) => {
-	res.redirect(
-		`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`,
-	);
-});
-
-app.get("/signin/callback", (req, res) => {
-	const { code } = req.query;
-	return res.json(code);
-});
-
 export { serverHttp };
