a774d341fd98de36e7d8e5e7708d42ed7a244439
BhryanS2 <bhryanstepenhen@gmail.com>
2022-01-15 12:48:48 -0300

    corrigindo bugs

--- dist/controllers/MessageController.js
+++ dist/controllers/MessageController.js
@@ -4,25 +4,25 @@ exports.MessageController = void 0;
 const MessageService_1 = require("../services/MessageService");
 class MessageController {
     async handle(req, res) {
-        const { email, message, firstName, lastName } = req.body;
-        if (!message || !firstName || !lastName || !email) {
+        const { Email, Message, FirstName, LastName } = req.body;
+        if (!Message || !FirstName || !LastName || !Email) {
             return res.status(400).json({
                 message: "Please fill all the fields",
                 fields: {
-                    message: !message ? "Message is required" : "",
-                    firstName: !firstName ? "First Name is required" : "",
-                    lastName: !lastName ? "Last Name is required" : "",
-                    email: !email ? "Email is required" : "",
+                    message: !Message ? "Message is required" : "",
+                    firstName: !FirstName ? "First Name is required" : "",
+                    lastName: !LastName ? "Last Name is required" : "",
+                    Email: !Email ? "Email is required" : "",
                 },
                 error: true,
             });
         }
         const messageService = new MessageService_1.MessageService();
         await messageService.execute({
-            Email: email,
-            Message: message,
-            FirstName: firstName,
-            LastName: lastName,
+            Email,
+            Message,
+            FirstName,
+            LastName,
         });
         return res.json({
             message: "Message sent successfully",
